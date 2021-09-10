098b6f3f5ac9b07a818925c1171f3f4e4a5c9343
Dhruv Vashist <53251202+dhruv12112000@users.noreply.github.com>
2021-09-10 20:32:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor(Math.random() * 6)+1;
 
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-var randomImageSource1 = "/images/" + randomDiceImage1;
+var randomImageSource1 = "images/" + randomDiceImage1;
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src" , randomImageSource1);
@@ -12,7 +12,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "/images/" + randomDiceImage2;
+var randomImageSource2 = "images/" + randomDiceImage2;
 
 
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
